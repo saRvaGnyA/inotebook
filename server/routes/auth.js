88205abf22a96a9e3925de6cbe53831a21f54df1
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2022-02-06 17:24:36 +0530

    basic validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const User = require("./../models/User");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 // Create a user using: POST "/api/auth". Doesn't require auth
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send("Hello");
-});
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+    body("email", "Enter a valid email").isEmail(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res
+          .status(400)
+          .json({ error: "Please enter unique email", message: err.message });
+      });
+  }
+);
 
 module.exports = router;
